fa048271c509463b210017da19eaf7bb78db9b9c
PauloHenriqueOliveiradeAlmeida <paulo333henrique00@gmail.com>
2025-06-19 20:37:12 -0300

    :sparkles: feat: use os file to configure

--- settings/settings.go
+++ settings/settings.go
@@ -2,7 +2,10 @@ package settings
 
 import (
 	"encoding/json"
+	"errors"
 	"os"
+	"path/filepath"
+	"runtime"
 )
 
 type Settings struct {
@@ -11,10 +14,28 @@ type Settings struct {
 	Theme     string `json:"theme"`
 }
 
-func ReadSettings(location string) Settings {
-	file, error := os.Open(location)
+func (this *Settings) Write(location string, filename string) error {
+	error := os.MkdirAll(location, 0700)
 	if error != nil {
-		panic(error)
+		return error
+	}
+
+	path := filepath.Join(location, filename)
+
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+
+	return json.NewEncoder(file).Encode(this)
+}
+
+func ReadSettings(location string, filename string) (*Settings, error) {
+	file, error := os.Open(filepath.Join(location, filename))
+	if error != nil {
+		return nil, errors.New("Configurações não encontradas, use 'zombie configure' para configurar")
 	}
 
 	settings := Settings{}
@@ -25,5 +46,19 @@ func ReadSettings(location string) Settings {
 		panic(error)
 	}
 
-	return settings
+	return &settings, nil
+}
+
+func GetSettingsPath() (string, string, error) {
+	home, error := os.UserHomeDir()
+	if error != nil {
+		return "", "", error
+	}
+
+	switch runtime.GOOS {
+	case "windows":
+		return filepath.Join(os.Getenv("AppData"), "zombie"), "settings.json", nil
+	default:
+		return filepath.Join(home, ".config", "zombie"), "settings.json", nil
+	}
 }
